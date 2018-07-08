345b6f72b4bd7fb0bc3e223f23f1dfe7b616bd17
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-09 11:29:50 +1200

    started to build foundation

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {Router} from 'react-router-dom'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <Router>
+        <div></div>
+      </Router>
+    )
+  }
+}
+
+export default App
