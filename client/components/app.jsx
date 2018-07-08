e11688f39571bcb303af6e7a27f2c1d0003e3d8c
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-09 11:33:24 +1200

    moved app.jsx contents

---
+++ client/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {Router} from 'react-router-dom'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <Router>
+        <div><h1>Hi</h1></div>
+      </Router>
+    )
+  }
+}
+
+export default App
