39e147fbe0addbd37efce51b2c715b3edd0e5ed8
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-09 12:05:13 +1200

    reddit api route and main.css files written

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('./express')
 const path = require('path')
 
-const something = require('./something')
+const auth = require('./auth')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, './public')))
 
-server.use('/api/v1/something', something)
+server.use('/api/v1/auth/register', auth)
 
 module.exports = server
