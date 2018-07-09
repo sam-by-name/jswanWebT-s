97bf745504f4dec66b25a18b9e9e705d4c138299
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-09 14:25:53 +1200

    pushing to master

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> ea5552e03fff6b199adb530f7c566e8f6d8ea817
 const path = require('path')
 
 module.exports = {
   mode: 'development',
-<<<<<<< HEAD
-  entry: path.join(__dirname, '/index.js'),
-  output: {
-    path: path.join(__dirname, '../server/public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
-=======
   entry: './client/index.js',
   output: {
     path: path.join(__dirname, '/public'),
@@ -32,14 +15,9 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
->>>>>>> ea5552e03fff6b199adb530f7c566e8f6d8ea817
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
   devtool: 'source-map'
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ea5552e03fff6b199adb530f7c566e8f6d8ea817
