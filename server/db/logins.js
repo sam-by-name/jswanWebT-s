fdfac8ad905749cc8f603d694b89ec73b7239a35
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-09 18:00:57 +1200

    cooking hash browns

--- server/db/logins.js
+++ server/db/logins.js
@@ -1,21 +1,25 @@
-const knex = require('knex')
-const config = require('../../knexfile').development
-const dbTest = knex(config)
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+const generate = require('../auth/hash')
+
+
 module.exports = {
   createUser,
   userExists
 }
 
 function createUser (username, password, conn) {
-  const db = conn || dbTest
+  const db = conn || connection
+  const hashVal = generate.generate(password)
   return db('logins')
-    .insert({username, hash: password})
+    .insert({username, hash: hashVal})
 }
 
 function userExists (username, conn) {
-  const db = conn || dbTest
+  const db = conn || connection
   return db('logins')
-    .count('id ad n')
+    .count('id as n')
     .where('username', username)
     .then(count => {
       console.log(count)
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,18 +2,32 @@ const express = require('express')
 const request = require('superagent')
 const db = require('../db/logins')
 const router = express.Router()
-
 router.use(express.json())
 
 
+// router.post('/register', (req,res) => {
+//   const {username, password} = req.body
+//   if (db.userExists(username)){
+//     return res.status(400).send('Username Taken')
+//   }
+//   else{
+//     db.createUser(username, password)
+//     return res.status(201)}
+// })
+
+
 router.post('/register', (req,res) => {
-  const {username, password} = req.body
-  if (db.userExists(username)){
-    return res.status(400).send('Username Taken')
-  }
-  else{
-    db.CreateUser(username, password)
-    return res.status(201)}
+  db.userExists(req.body.username)
+    .then(exists => {
+      if (exists) {
+        return res.status(400).send({ message: 'User exists' })
+      }
+      db.createUser(req.body.username, req.body.password)
+        .then(() => res.status(201).end())
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message })
+    })
 })
 
 module.exports = router
