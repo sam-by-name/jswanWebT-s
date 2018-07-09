61f02d5365ca33b1804d60dea0c9aa1674aca6f6
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-09 12:22:49 +1200

    actions folder with reddit actions added and reddit routes made

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('./express')
 const path = require('path')
 
-const auth = require('./auth')
+const auth = require('./routes/authRoutes')
+const reddit = require('./routes/redditRoutes')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/auth/register', auth)
+server.use('/api/vi/reddit', reddit)
 
 module.exports = server
