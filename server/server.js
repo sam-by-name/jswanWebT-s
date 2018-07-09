2978b696970e118da78bf940843899c0ac54c447
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-09 14:19:04 +1200

    creating routes for auth

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require('./express')
+const express = require('express')
 const path = require('path')
 
-const auth = require('./auth')
+const auth = require('auth')
 
 const server = express()
 
+server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/auth/register', auth)
